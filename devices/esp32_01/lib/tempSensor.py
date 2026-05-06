e864a0fd6639114dfa5fd0c30e6aea62801966c6
Theodor <thgr0003@stud.kea.dk>
2026-05-06 15:56:58 +0200

    Change the structure of the device code folders

---
+++ devices/esp32_01/lib/tempSensor.py
@@ -0,0 +1,3 @@
+class TempSensor:
+    def __init__(self):
+        ...
--- devices/esp32_01/main.py
+++ devices/esp32_01/main.py
@@ -1,62 +1,9 @@
 from machine import I2C, Pin
 from lib.battery_status import BatteryStatus
-from mqtt import MQTT
+from devices.esp32_01.lib.mqtt import MQTT
 from time import sleep, time
 from random import randint
 import ujson
-import urequests
-import ssl, socket
-
-CURRENT_VERSION = "1.0.1"
-HOST = "192.168.4.1"
-PORT = 5001
-
-def make_request(path):
-    addr = socket.getaddrinfo(HOST, PORT)[0][-1]
-    sock = socket.socket()
-    sock.connect(addr)
-    
-    # wrap_socket is available in your build
-    sock = ssl.wrap_socket(
-        sock,
-        server_hostname=HOST,
-        cert=open("client.crt", "rb").read(),    # mTLS client cert
-        key=open("client.key", "rb").read(),      # mTLS client key
-        cadata=open("ca.crt", "rb").read(),       # verify server
-        cert_reqs=ssl.CERT_REQUIRED               # enforce server verification
-    )
-    
-    request = (
-        f"GET {path} HTTP/1.0\r\n"
-        f"Host: {HOST}\r\n"
-        f"Connection: close\r\n"
-        f"\r\n"
-    )
-    sock.write(request.encode())
-    
-    response = b""
-    while True:
-        chunk = sock.read(1024)
-        if not chunk:
-            break
-        response += chunk
-    
-    sock.close()
-    
-    headers_raw, body = response.split(b"\r\n\r\n", 1)
-    headers = {}
-    for line in headers_raw.split(b"\r\n")[1:]:
-        if b":" in line:
-            k, v = line.split(b":", 1)
-            headers[k.strip().decode()] = v.strip().decode()
-    
-    return headers, body
-
-def check_update():
-    headers, body = make_request("/ota/version")
-    latest = ujson.loads(body)["version"]
-    print(f"Current: {CURRENT_VERSION}, Latest: {latest}")
-    return latest != CURRENT_VERSION
 
 def sub_cb(topic, msg):
     try:
